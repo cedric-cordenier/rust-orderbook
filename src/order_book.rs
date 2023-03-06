bd8897a6ffc5e2a5d1bbca83ebbced22280242de
Cedric Cordenier <cedric@gyro.finance>
2023-03-06 12:16:06 +0000

    Implement add command

--- src/order_book.rs
+++ src/order_book.rs
@@ -1,45 +1,119 @@
 mod order_book {
+    use crate::tree::Tree;
     use serde::Deserialize;
-    use tree;
+    use std::cmp::Ordering;
 
-    #[derive(Debug)]
+    enum OrderType {
+        Buy,
+        Sell
+    }
+
+    #[derive(Clone)]
+    pub struct Order {
+        price: String,
+        size: String,
+    }
+
+    #[derive(Clone)]
+    pub struct Limit {
+        price: String,
+        orders: Vec<Order>,
+    }
+
+    impl Eq for Limit {}
+
+    impl PartialEq for Limit {
+        fn eq(&self, other: &Self) -> bool {
+            self.price == other.price
+        }
+    }
+
+    impl Ord for Limit {
+        fn cmp(&self, other: &Self) -> Ordering {
+            self.price.cmp(&other.price)
+        }
+    }
+
+    impl PartialOrd for Limit {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
     pub struct Book {
-        buy_tree: Tree<Order>,
-        sell_tree: Tree<Order>,
+        buy_tree: Tree<Limit>,
+        sell_tree: Tree<Limit>,
     }
 
     #[derive(Deserialize)]
-    #[derive(Clone)]
-    #[derive(Debug)]
-    struct Order {
+    struct AggregatedOrder {
         price: String,
         size: String,
-        num_orders: usize
+        num_orders: usize,
     }
 
     #[derive(Deserialize)]
     struct OrderBookResponse {
-        bids: Vec<Order>,
-        asks: Vec<Order>
+        bids: Vec<AggregatedOrder>,
+        asks: Vec<AggregatedOrder>,
+    }
+
+    fn to_limits<'a>(orders: Vec<AggregatedOrder>) -> Vec<Limit> {
+        orders
+            .iter()
+            .map(|n| {
+                let os = vec![
+                    Order {
+                        price: n.price.clone(),
+                        size: n.size.clone()
+                    };
+                    n.num_orders
+                ];
+                Limit {
+                    price: n.price.clone(),
+                    orders: os,
+                }
+            })
+            .collect()
     }
 
     pub fn get() -> Book {
         let client = reqwest::blocking::Client::new();
-        let body = client.get(
-            "https://api.exchange.coinbase.com/products/ETH-GBP/book?level=2",
-        )
-        .header(reqwest::header::USER_AGENT, "Rust OrderBook")
-        .send()
-        .unwrap()
-        .json::<OrderBookResponse>()
-        .unwrap();
+        let body = client
+            .get("https://api.exchange.coinbase.com/products/ETH-GBP/book?level=2")
+            .header(reqwest::header::USER_AGENT, "Rust OrderBook")
+            .send()
+            .unwrap()
+            .json::<OrderBookResponse>()
+            .unwrap();
 
-        let mut buy_tree = Tree { tree: Vec::new() };
-        buy_tree.populate_from_sorted(None, body.bids.as_slice());
- 
-        let mut sell_tree = Tree { tree: Vec::new() };
-        sell_tree.populate_from_sorted(None, body.asks.as_slice());
-        let book = Book{buy_tree, sell_tree};
+        let buy_limits = to_limits(body.bids);
+        let mut buy_tree = Tree::new();
+        buy_tree.populate_from_sorted(None, &buy_limits.as_slice());
+
+        let mut sell_tree = Tree::new();
+        let sell_limits = to_limits(body.asks);
+        sell_tree.populate_from_sorted(None, &sell_limits.as_slice());
+        let book = Book {
+            buy_tree,
+            sell_tree,
+        };
         book
+    }
+
+    impl Book {
+        fn add(&mut self, order_type: OrderType, order: Order) {
+            let target = match order_type {
+                OrderType::Buy => &mut self.buy_tree,
+                OrderType::Sell => &mut self.sell_tree,
+            };
+
+            let limit = Limit {
+                price: order.price.clone(),
+                orders: Vec::new(),
+            };
+            let l = target.add(limit, 0);
+            l.orders.push(order);
+        }
     }
 }
--- src/tree.rs
+++ src/tree.rs
@@ -1,60 +1,173 @@
-mod tree {
-    pub struct Node<T> {
-        idx: usize,
-        left_idx: Option<usize>,
-        right_idx: Option<usize>,
-        parent_idx: Option<usize>,
-        val: T
-    }
+use std::slice::Iter;
 
-    pub struct Tree<T> {
-        tree: Vec<Node<T>>
-    }
+pub struct Node<T> {
+    idx: usize,
+    left_idx: Option<usize>,
+    right_idx: Option<usize>,
+    parent_idx: Option<usize>,
+    pub val: T
+}
 
-    impl<T: Clone> Tree<T> {
-        pub fn populate_from_sorted(&mut self, parent_idx: Option<usize>, sorted: &[T]) -> Option<usize> {
-            if sorted.len() == 0 {
-                return None;
-            }
+pub struct Tree<T> {
+    tree: Vec<Node<T>>
+}
 
-            let mid_idx = sorted.len() / 2;
-
-            let curr_idx = self.tree.len();
-            let node = Node {
-                idx: curr_idx,
-                left_idx: None,
-                right_idx: None,
-                parent_idx,
-                val: sorted[mid_idx].clone()
-            };
-            self.tree.push(node);
-
-            let left_idx = self.populate_from_sorted(Some(curr_idx), &sorted[..mid_idx]);
-            self.tree[curr_idx].left_idx = left_idx;
-
-            let right_idx = self.populate_from_sorted(Some(curr_idx), &sorted[mid_idx+1..]);
-            self.tree[curr_idx].right_idx = right_idx;
-
-            Some(curr_idx)
+impl<T: Clone + Ord> Tree<T> {
+    pub fn populate_from_sorted(&mut self, parent_idx: Option<usize>, sorted: &[T]) -> Option<usize> {
+        if sorted.len() == 0 {
+            return None;
         }
 
-        pub fn new() -> Tree<T> {
-            Tree::<T> {
-                tree: Vec::new()
+        let mid_idx = sorted.len() / 2;
+
+        let curr_idx = self.tree.len();
+        let node = Node {
+            idx: curr_idx,
+            left_idx: None,
+            right_idx: None,
+            parent_idx,
+            val: sorted[mid_idx].clone()
+        };
+        self.tree.push(node);
+
+        let left_idx = self.populate_from_sorted(Some(curr_idx), &sorted[..mid_idx]);
+        self.tree[curr_idx].left_idx = left_idx;
+
+        let right_idx = self.populate_from_sorted(Some(curr_idx), &sorted[mid_idx+1..]);
+        self.tree[curr_idx].right_idx = right_idx;
+
+        Some(curr_idx)
+    }
+
+    pub fn val_at(& self, idx: Option<usize>) -> Option<T> {
+        match self.at(idx) {
+            None => {
+                return None;
             }
+            Some(i) => {
+                return Some(i.val.clone());
+            }
+        }
+    }
+
+    pub fn at(& self, idx: Option<usize>) -> Option<&Node<T>> {
+        match idx {
+            None => {
+                return None;
+            },
+            Some(i) => {
+                if i >= self.tree.len() {
+                    return None;
+                }
+
+                Some(&self.tree[i])
+            }
+        }
+    }
+
+    pub fn add(&mut self, val: T, curr_idx: usize) -> &mut T {
+        if self.tree.len() == 0 {
+            self.tree.push(
+                Node {
+                    idx: 0,
+                    left_idx: None,
+                    right_idx: None,
+                    parent_idx: None,
+                    val,
+                }
+            );
+            return &mut self.tree[0].val
+        }
+
+        if self.tree[curr_idx].val == val {
+            return &mut self.tree[curr_idx].val;
+        } else if self.tree[curr_idx].val < val {
+            match self.tree[curr_idx].left_idx {
+                None => {
+                    let next_insert_idx = self.tree.len();
+                    self.tree.push(
+                        Node {
+                            idx: next_insert_idx,
+                            left_idx: None,
+                            right_idx: None,
+                            parent_idx: Some(curr_idx),
+                            val
+                        }
+                    );
+                    self.tree[curr_idx].left_idx = Some(next_insert_idx);
+                    return &mut self.tree[next_insert_idx].val;
+                }
+                Some(i) => {
+                    return self.add(val, i);
+                }
+            }
+        } else {
+            match self.tree[curr_idx].right_idx {
+                None => {
+                    let next_insert_idx = self.tree.len();
+                    self.tree.push(
+                        Node {
+                            idx: next_insert_idx,
+                            left_idx: None,
+                            right_idx: None,
+                            parent_idx: Some(curr_idx),
+                            val
+                        }
+                    );
+                    self.tree[curr_idx].right_idx = Some(next_insert_idx);
+                    return &mut self.tree[next_insert_idx].val;
+                }
+                Some(i) => {
+                    return self.add(val, i);
+                }
+            }
+        }
+    }
+
+    pub fn iter(&mut self) -> Iter<Node<T>> {
+        self.tree.iter()
+    }
+
+    pub fn new() -> Tree<T> {
+        Tree::<T> {
+            tree: Vec::new()
         }
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::tree::*;
+#[test]
+fn populate_tree_from_sorted() {
+    let mut t: Tree<usize> = Tree::new();
+    let arr: &[usize] = &[1, 2, 3, 4, 5, 6];
+    t.populate_from_sorted(None, arr);
+    let got_vec = t.iter().map(|n| n.val).collect::<Vec<usize>>();
+    assert_eq!(vec![4, 2, 1, 3, 6, 5], got_vec);
+}
 
-    #[test]
-    fn populate_tree_from_sorted() {
-        let mut t: Tree<usize> = Tree::new();
-        let arr: &[usize] = &[1, 2, 3, 4, 5, 6];
-        t.populate_from_sorted(None, arr);
-        assert_eq!(true, false);
-    }
+#[test]
+fn tree_contains_correct_left_and_right_idxs() {
+    let mut t: Tree<usize> = Tree::new();
+    let arr: &[usize] = &[1, 2, 3, 4, 5, 6];
+    t.populate_from_sorted(None, arr);
+
+    // Our tree should look like:
+    //              4
+    //      2           6
+    //  1       3   5
+
+    let first_el = &t.tree[0];
+    // first_el has no parent as it's the root
+    assert_eq!(first_el.parent_idx, None);
+    assert_eq!(t.val_at(first_el.left_idx).unwrap(), 2);
+    assert_eq!(t.val_at(first_el.right_idx).unwrap(), 6);
+
+    let second_el = &t.tree[1];
+    assert_eq!(second_el.parent_idx.unwrap(), 0);
+    assert_eq!(t.val_at(second_el.left_idx).unwrap(), 1);
+    assert_eq!(t.val_at(second_el.right_idx).unwrap(), 3);
+
+    let third_el = &t.tree[4];
+    assert_eq!(third_el.parent_idx.unwrap(), 0);
+    assert_eq!(t.val_at(third_el.left_idx).unwrap(), 5);
+    assert_eq!(t.val_at(third_el.right_idx), None);
 }
